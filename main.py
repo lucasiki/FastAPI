ddf6c3c3217ded65896db96311933979aba3e123
lucasscan <lucasscandian@hotmail.com>
2022-04-09 08:12:31 -0700

    deploy with success

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from a2wsgi import ASGIMiddleware
 
 app = FastAPI()
 
 @app.get('/')
 def appget():
     return {"text": "hellow mama"}
+    
+    
+    
+    
+    
+    
+wsgi_app = ASGIMiddleware(app)
+    
